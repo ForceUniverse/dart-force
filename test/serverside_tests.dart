5b9632c3e644d927dda0eecd4b08fbd5a169e738
joris hermans <JHermans85@hotmail.com>
2013-12-19 23:49:41 +0100

    creating basic test for force framework

--- test/serverside_tests.dart
+++ test/serverside_tests.dart
@@ -1,8 +1,28 @@
+import 'package:unittest/unittest.dart';
 import 'package:force/force_serverside.dart';
-import 'package:force/force_common.dart';
+import 'dart:convert';
 
 void main() {
   print("Hello, World!"); 
   
+  test('force basic messageDispatcher test', () {
+    ForceServer fs = new ForceServer();
+    var request = "req:";
+    var profileName = 'chatName';
+    
+    var sendingPackage =  {'request': request,
+                           'type': { 'name' : 'normal'},
+                           'profile': {'name' : profileName},
+                           'data': { 'key' : 'value', 'key2' : 'value2' }};
+
+    fs.on(request, expectAsync2((e, sendable) {
+        expect(e.profile['name'], profileName);
+        expect(e.json['key'], 'value');
+        expect(e.json['key2'], 'value2');
+    }));
+
+    
+    fs.handleMessages("id:bla", JSON.encode(sendingPackage));
+  });
   
 }
