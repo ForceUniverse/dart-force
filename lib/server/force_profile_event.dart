e8ab0700f73679b4b16d7b992dca586728c02857
joris hermans <JHermans85@hotmail.com>
2013-12-23 12:26:02 +0100

    new and changing profile properties

--- lib/server/force_profile_event.dart
+++ lib/server/force_profile_event.dart
@@ -5,10 +5,23 @@ class ForceProfileEvent {
   ForceProfileType type;
   String wsId;
   dynamic profileInfo;
+  ForceProperty property;
   
-  ForceProfileEvent(this.type, this.wsId, this.profileInfo); 
+  ForceProfileEvent(this.type, this.wsId, this.profileInfo, {property: null}) {
+    this.property = property;
+  } 
   
   toString() => "[$wsId] $type $profileInfo";
+}
+
+class ForceProperty {
+  
+  String key;
+  String value;
+  
+  ForceProperty(this.key, this.value);
+  
+  toString() => "$key - $value";
 }
 
 class ForceProfileType {
@@ -18,6 +31,8 @@ class ForceProfileType {
 
   static const New = const ForceProfileType('New');
   static const Removed = const ForceProfileType('Removed');
+  static const NewProperty = const ForceProfileType('NewProperty');
+  static const ChangedProperty = const ForceProfileType('ChangedProperty');
   
   toString() => "$_type";
 }
