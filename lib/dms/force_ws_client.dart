8902a6a5f6f1fa4670f05085b50b3f4296dcdb60
joris hermans <JHermans85@hotmail.com>
2013-12-26 22:36:23 +0100

    adjusting client code

--- lib/dms/force_ws_client.dart
+++ lib/dms/force_ws_client.dart
@@ -1,12 +1,7 @@
 part of dart_force_client_lib;
 
 class ForceClient extends ForceBaseMessageSendReceiver with ClientSendable {
-  static const Duration RECONNECT_DELAY = const Duration(milliseconds: 500);
-  
-  bool _connectPending = false;
-  WebSocket webSocket;
-  
-  StreamController<ForceConnectEvent> _connectController;
+  WebSocketWrapper webSocketWrapper;
   
   ForceMessageDispatcher _messageDispatcher;
   
@@ -15,50 +10,22 @@ class ForceClient extends ForceBaseMessageSendReceiver with ClientSendable {
   var _profileInfo = {};
   
   ForceClient({wsPath: "/ws"}) {
-    _connectController = new StreamController<ForceConnectEvent>();
     _messageDispatcher = new ForceMessageDispatcher(this);
     this.wsPath = wsPath;
+    
+    this.webSocketWrapper = new WebSocketWrapper('ws://${Uri.base.host}:${Uri.base.port}$wsPath');
   }
   
   void connect() {
-    print("try to connect with the server ...");
-    _connectPending = false;
-    //_connectController = new StreamController<ForceConnectEvent>();
-    webSocket = new WebSocket('ws://${Uri.base.host}:${Uri.base.port}$wsPath');
-    webSocket.onOpen.first.then((_) {
-      _onConnected();
-      webSocket.onClose.first.then((_) {
-        print("Connection disconnected to ${webSocket.url}");
-        _onDisconnected();
-      });
-    });
-    webSocket.onError.first.then((_) {
-      print("Failed to connect to ${webSocket.url}. "
-            "Please run bin/server.dart and try again.");
-      _onDisconnected();
-    });
-  }
-  
-  void _onConnected() {
-    print("connected!");
-    _connectController.add(new ForceConnectEvent("connected"));
-    print("wicked new ForceEvent no ? ? !");
-    webSocket.onMessage.listen((e) {
-      _messageDispatcher.onMessageDispatch(onInnerMessage(e.data));
-    });
-  }
-  
-  void _onDisconnected() {
-    print("disconnected!");
-    _connectController.add(new ForceConnectEvent("disconnected"));
-    if (_connectPending) return;
-    _connectPending = true;
-    new Timer(RECONNECT_DELAY, connect);
+   this.webSocketWrapper.connect();
+   this.webSocketWrapper.onMessage.listen((e) {
+     _messageDispatcher.onMessageDispatch(onInnerMessage(e.data));
+   });
   }
   
   void on(String request, MessageReceiver vaderMessageController) {
     _messageDispatcher.register(request, vaderMessageController);
   }
    
-  Stream<ForceConnectEvent> get onConnecting => _connectController.stream;
+  Stream<ForceConnectEvent> get onConnecting =>  this.webSocketWrapper.onConnecting;
 }
