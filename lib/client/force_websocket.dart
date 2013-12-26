bada6ab8b1902bdffa21d97d96b03d80f7775228
joris hermans <JHermans85@hotmail.com>
2013-12-27 00:44:56 +0100

    socket support

--- lib/client/force_websocket.dart
+++ lib/client/force_websocket.dart
@@ -1,19 +1,12 @@
 part of dart_force_client_lib;
 
 class ForceWebSocket {
- 
-  String _url;
   
-  AbstractSocket _wsImpl;
-  
-  ForceWebSocket(this._url) {
+  static AbstractSocket createSocket(String url) {
     if (WebSocket.supported) {
-      _wsImpl = new WebSocketWrapper(_url);
+      return new WebSocketWrapper(url);
     } else {
       // use long polling!
     }
   }
-  
-  AbstractSocket get socket => _wsImpl;
-  
 }
