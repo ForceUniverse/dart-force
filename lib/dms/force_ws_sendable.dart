64b8d956dd4468555ea800a1720e6ecbd24deb2e
joris hermans <JHermans85@hotmail.com>
2013-12-28 14:10:47 +0100

    adding more logging

--- lib/dms/force_ws_sendable.dart
+++ lib/dms/force_ws_sendable.dart
@@ -43,6 +43,7 @@ class ClientSendable implements Sender {
   
   void _send(sendingPackage) {
     if (socket != null && socket.isOpen()) {
+      print('send package to the server');
       socket.send(JSON.encode(sendingPackage));
     } else {
       print('WebSocket not connected, message $sendingPackage not sent');
