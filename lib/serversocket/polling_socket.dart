57d359fe3b9e62cfa3968f05e4f67f88e114007a
joris hermans <JHermans85@hotmail.com>
2013-12-29 17:25:04 +0100

    adding more logic in polling socket

---
+++ lib/serversocket/polling_socket.dart
@@ -0,0 +1,28 @@
+part of dart_force_server_lib;
+
+class DataPackage {
+  var data;
+  DataPackage(this.data);
+}
+
+class PollingSocket extends Socket {
+  
+  List<DataPackage> messages = new List<DataPackage>();
+  Completer completer = new Completer.sync();
+  
+  Future done() {
+    return completer.future;
+  }
+  
+  bool isClosed() {
+    return false;
+  }
+  
+  void close() {
+    completer.complete(const []);
+  }
+
+  void add(data) {
+    messages.add(data);
+  }
+}
