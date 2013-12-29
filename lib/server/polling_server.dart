aa84b29ca0497b9f72fc3a3260a7004707713211
joris hermans <JHermans85@hotmail.com>
2013-12-29 12:08:32 +0100

    adding polling server
    he is going to controll polling on the serverside

---
+++ lib/server/polling_server.dart
@@ -0,0 +1,47 @@
+part of dart_force_server_lib;
+
+class PollingServer {
+  
+  final Logger log = new Logger('PollingServer');
+  
+  Router router;
+  String wsPath;
+  
+  PollingServer(this.router, this.wsPath) {
+    print('start long polling server ... $wsPath/polling');
+    router.serve('$wsPath/polling', method: "GET").listen(polling);
+    router.serve('$wsPath/polling', method: "POST").listen(sendedData);
+  }
+  
+  void polling(HttpRequest req) {
+    print("get data from longpolling!");
+    
+    UTF8.decodeStream(req).then((content) {
+      final params = Uri.splitQueryString(content);
+      print(params['id']);
+    });
+    
+    var response = req.response;
+    var dynamic = {"status" : "ok"};
+    String data = JSON.encode(dynamic);
+    response
+    ..statusCode = 200
+    ..headers.contentType = new ContentType("application", "json", charset: "utf-8")
+    ..headers.contentLength = data.length
+    ..write(data)
+      ..close();
+  }
+  
+  void sendedData(HttpRequest req) {
+    var response = req.response;
+    var dynamic = {"status" : "ok"};
+    String data = JSON.encode(dynamic);
+    response
+    ..statusCode = 200
+    ..headers.contentType = new ContentType("application", "json", charset: "utf-8")
+    ..headers.contentLength = data.length
+    ..write(data)
+      ..close();
+  }
+  
+}
