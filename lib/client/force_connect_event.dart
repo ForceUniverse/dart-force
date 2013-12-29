52568af46954b4df7a0e2db8bfccae6ff6e4a140
joris hermans <JHermans85@hotmail.com>
2013-12-29 13:45:01 +0100

    rename dms into client

---
+++ lib/client/force_connect_event.dart
@@ -0,0 +1,9 @@
+part of dart_force_client_lib;
+
+class ForceConnectEvent {
+  
+  String type;
+  
+  ForceConnectEvent(this.type);
+  
+}
---
+++ lib/client/force_ws_client.dart
@@ -0,0 +1,31 @@
+part of dart_force_client_lib;
+
+class ForceClient extends ForceBaseMessageSendReceiver with ClientSendable {
+  AbstractSocket socket;
+  
+  ForceMessageDispatcher _messageDispatcher;
+  
+  String wsPath;
+  
+  var _profileInfo = {};
+  
+  ForceClient({wsPath: "/ws", usePolling: false}) {
+    _messageDispatcher = new ForceMessageDispatcher(this);
+    this.wsPath = wsPath;
+    
+    this.socket = ForceWebSocket.createSocket('${Uri.base.host}:${Uri.base.port}$wsPath', usePolling: usePolling);
+  }
+  
+  void connect() {
+   this.socket.connect();
+   this.socket.onMessage.listen((e) {
+     _messageDispatcher.onMessageDispatch(onInnerMessage(e.data));
+   });
+  }
+  
+  void on(String request, MessageReceiver vaderMessageController) {
+    _messageDispatcher.register(request, vaderMessageController);
+  }
+   
+  Stream<ForceConnectEvent> get onConnecting =>  this.socket.onConnecting;
+}
