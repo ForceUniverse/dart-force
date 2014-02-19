3b6ac655d2f2bab1c89be1371681493800cff462
joris hermans <JHermans85@hotmail.com>
2014-02-19 22:06:22 +0100

    adding new method to force serveable

--- lib/server/force_serveable.dart
+++ lib/server/force_serveable.dart
@@ -8,6 +8,10 @@ class Serveable {
     return _basicServer.router.serve(name);
   }
   
+  Stream<HttpRequest> serveByPattern(Pattern name) {
+    return _basicServer.router.serve(name);
+  }
+  
   void serveFile(String fileName, HttpRequest request) {
     Uri fileUri = Platform.script.resolve(fileName);
     _basicServer.virDir.serveFile(new File(fileUri.toFilePath()), request);
