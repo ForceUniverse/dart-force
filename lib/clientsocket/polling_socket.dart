40b56fc3101db36e016145c79ef4e8557ac9a387
joris hermans <JHermans85@hotmail.com>
2014-03-18 22:37:44 +0100

    remove alert
    testing on IE9

--- lib/clientsocket/polling_socket.dart
+++ lib/clientsocket/polling_socket.dart
@@ -34,7 +34,6 @@ class PollingSocket extends Socket {
   
   void polling() {
     print('polling to ... http://$_url/polling/?pid=$_uuid');
-    window.alert("polling");
     HttpRequest.getString('http://$_url/polling/?pid=$_uuid').then(processString);
   }
   
