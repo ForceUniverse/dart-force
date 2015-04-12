9ce2227c176321e024c2a28312967736f1821b8e
joris hermans <JHermans85@hotmail.com>
2015-04-12 20:45:16 +0200

    adapt to do some test on other dart frameworks, plugin in force

--- lib/serversocket/stream_socket.dart
+++ lib/serversocket/stream_socket.dart
@@ -15,7 +15,7 @@ class StreamSocket extends ForceSocket {
     
     _messageController = new StreamController<MessageEvent>();
     
-    subscription = this.stream.transform(UTF8.decoder).listen((data) {
+    subscription = this.stream.listen((data) {
       _messageController.add(new MessageEvent(request, data));
     });
     subscription.onDone(() {
