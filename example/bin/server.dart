7017d8ee96c7ff10df83b409108813f6a2eed593
joris hermans <JHermans85@hotmail.com>
2015-05-06 21:44:27 +0200

    update example to async await

--- example/bin/server.dart
+++ example/bin/server.dart
@@ -4,7 +4,7 @@ import "package:force/force_serverside.dart";
 
 part 'receivers/justareceiver.dart';
 
-void main() {
+main() async {
   
   ForceServer fs = new ForceServer(port: 3030, 
                                    clientFiles: "../build/web/", 
@@ -12,11 +12,12 @@ void main() {
   
   fs.setupConsoleLog();
   
-  fs.start().then((_) {
-    fs.on("add", (message, sender) {
-          fs.send("update", message.json);
-      });
+  await fs.start();
+    
+  fs.on("add", (message, sender) {
+       fs.send("update", message.json);
   });
+  
   
   // add serversocket implementation into the game! server 2 server communication 
   Connector connector = new ServerSocketConnector();
