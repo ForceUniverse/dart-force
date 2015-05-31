4770494476991822d4fb5989bc9565c5bf4b331f
joris hermans <JHermans85@hotmail.com>
2015-05-31 20:43:49 +0200

    add a test lib, so you can test the ws protocols

--- test/flow_tests.dart
+++ test/flow_tests.dart
@@ -1,5 +1,6 @@
 import 'package:test/test.dart';
 import 'package:force/force_serverside.dart';
+import 'package:force/test.dart';
 import 'dart:convert';
 import 'dart:async';
 
@@ -55,66 +56,5 @@ void main() {
       
       force.send(request, data);
     });
-  
-}
-
-/// A Testing helper class to construct everything you need to test end2end
-class TestForce {
-    TwoWaySocket _twoWaySocket = new TwoWaySocket();
-    BaseForceClient forceClient;    
-  
-    TestForce(Force force) {
-        forceClient = new TestForceClient(_twoWaySocket.clientSocket);
-        force.handle(_twoWaySocket.serverSocket);
-    }
-}
-
-/// Test interface for ForceClient 
-class TestForceClient extends BaseForceClient with ClientSendable {
-    ForceSocket socket;
-    
-    TestForceClient(this.socket) {
-      clientContext = new ForceClientContext(this);  
-      
-      this.messenger = new ServerMessenger(socket);
-    }
-    
-    void connect() {
-       socket.onMessage.listen((e) {
-         clientContext.protocolDispatchers.dispatch_raw(e.data);
-       });
-    }
-  
-}
-
-/// Two way socket implementation, to mock the connection between to sockets 
-class TwoWaySocket {
-  
-  ForceSocket clientSocket;
-  ForceSocket serverSocket;
-  dynamic _client_sended_data;
-  dynamic _server_sended_data;
-  
-  TwoWaySocket() {
-    StreamController clientStream = new StreamController.broadcast();
-    clientSocket = new StreamSocket.fromController(clientStream);
-    
-    StreamController serverStream = new StreamController.broadcast();
-    serverSocket = new StreamSocket.fromController(serverStream);
-    
-    clientStream.stream.listen((data) {
-      if (data != _server_sended_data) {
-        serverStream.add(data);
-        _client_sended_data = data;
-      }
-    });
-    
-    serverStream.stream.listen((data) {
-      if (data != _client_sended_data) {
-        clientStream.add(data);
-        _server_sended_data = data;
-      }
-    });
-  }
   
 }
