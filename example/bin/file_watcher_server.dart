7a6461e9c95a1c942664ff0fe62b85f525086567
joris hermans <joris.hermans@goudengids.be>
2016-03-11 12:45:10 +0100

    remove unused imports

--- example/bin/file_watcher_server.dart
+++ example/bin/file_watcher_server.dart
@@ -2,13 +2,12 @@ library dart_force_todo_server2;
 
 import "package:force/force_serverside.dart";
 
-import "dart:async";
 import "dart:io";
 
 import 'package:logging/logging.dart' show Logger, Level, LogRecord;
 
 void main() {
-  
+
   Logger.root.level = Level.INFO;
   Logger.root.onRecord.listen((LogRecord rec) {
           if (rec.level >= Level.SEVERE) {
@@ -18,22 +17,22 @@ void main() {
             print('${rec.level.name}: ${rec.time}: ${rec.message}');
           }
         });
-  
+
   ForceClient fc = new ForceClient();
-    
+
   fc.on("update", (fme, sender) {
       print("todo: ${fme.json["todo"]}");
   });
-  
+
   var pathTo = Platform.script.resolve("../store/").toFilePath();
   var uriKey = new Uri.file(pathTo).resolve("todo.txt");
   File file = new File(uriKey.toFilePath());
   Directory dir = new Directory(pathTo);
-  
+
   String content = "";
-  
+
   fc.connect().then((_) {
-     // readlines    
+     // readlines
      dir.watch().listen((FileSystemEvent fse) {
        if (fse.type == FileSystemEvent.MODIFY && !fse.isDirectory) {
          String newContent = file.readAsStringSync();
@@ -48,5 +47,5 @@ void main() {
        }
      });
   });
-  
+
 }
